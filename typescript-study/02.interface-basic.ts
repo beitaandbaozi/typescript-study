7588b13a191155ad04c1c7e9d2fdaacbab1c51d5
beitaandbaozi <2280496040@qq.com>
2022-05-20 13:01:42 +0800

    object(interface)初探

---
+++ typescript-study/02.interface-basic.ts
@@ -0,0 +1,24 @@
+interface Person {
+  // 属性前加上 readonly 表示只读 不可更改
+  readonly id: number
+  name: string
+  age: number
+}
+
+let beiTa: Person = {
+  id: 1,
+  name: 'beiTa',
+  age: 22
+}
+// beiTa.id = 2    //无法分配到 "id" ，因为它是只读属性
+
+interface Car {
+  name: string
+  // 属性加上 ？ 表示可选
+  age?: number
+}
+
+let bmw: Car = {
+  name: 'bmw',
+  age: 22
+}

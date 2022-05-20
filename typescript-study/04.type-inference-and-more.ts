406fd455b355ebebd5f9ab4ce2ebeb9f2f7f6d84
beitaandbaozi <2280496040@qq.com>
2022-05-20 14:28:55 +0800

    feat:类型推论、联合类型和类型断言

---
+++ typescript-study/04.type-inference-and-more.ts
@@ -0,0 +1,27 @@
+// type inference
+let str = 'str'
+
+// union types  联合类型
+let numberOrString: number | string
+
+function getLength(input: number | string): number {
+  // 类型断言
+  const str = input as string
+  if (str.length) {
+    return str.length
+  } else {
+    const number = input as number
+    return number.toString().length
+  }
+
+}
+
+//  type guard
+// 在不同的条件中智能的缩小了范围
+function getLength2(input: number | string): number {
+  if (typeof input == 'string') {
+    return input.length
+  } else {
+    return input.toString().length
+  }
+}
